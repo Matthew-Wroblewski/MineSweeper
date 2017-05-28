0f87340fa6494456a70a78896a0ad9316245ad7a
Matthew-Wroblewski <wroblewski.m@zoho.com>
2017-05-28 02:27:52 -0400

    added bombs

--- js/main.js
+++ js/main.js
@@ -1,25 +1,56 @@
 
 $( document ).ready(function() {
-    console.log( "ready!" );
+  console.log( "ready!" );
+  /// 1/5 mines
 
-    function drawBoard () {
-
-      var i, j, fieldElement;
-
-         for (i = 0; i < 7; i++)
-         {
+  numMines = 7;
+  plantedMines = 0;
+  var boardMatrix = [];
 
 
-             for (j = 0; j < 5; j++)
-             {
-                 $(".container").append("<button type='button'> </button>");
+  function drawBoard () {
 
+    var i, j, fieldElement;
+    for (i = 0; i < 7; i++)
+    {
+      $("table").append("<tr class='board-row-" + i + "'></tr>");
+      boardMatrix[i] = [];
 
-             }
-               $(".container").append("<br>");
+      for (j = 0; j < 5; j++)
+      {
+        $(".board-row-" + i + "").append("<td class='cell-row-" + i + " cell-column-" + j + "'> 1 </td>");
+        boardMatrix[i][j] = [];
+        boardMatrix[i][j] = false;
+      }
+      $("tr").append("</tr>");
 
-         }
     }
 
-    drawBoard();
+
+  }
+
+  function plantMines () {
+    var i, row, col;
+    do {
+      row = Math.floor(Math.random() * 7);
+      col = Math.floor(Math.random() * 5);
+      if (boardMatrix[row][col] == true)
+        continue;
+      boardMatrix[row][col] = true;
+      $(".cell-row-"+row+".cell-column-"+col+"").addClass("bomb");
+
+
+      plantedMines++;
+    }
+    while (numMines > plantedMines);
+
+  }
+
+  $('td').click(function(){
+    var colIndex = $(this).prevAll().length;
+    var rowIndex = $(this).parent('tr').prevAll().length;
+});
+
+  drawBoard();
+  plantMines();
 });
