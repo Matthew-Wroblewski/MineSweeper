f45a406836d13220218c5c1985d6ce5e2301a680
Matthew-Wroblewski <wroblewski.m@zoho.com>
2017-05-29 03:34:51 -0400

    Board Matrix additions

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
 
       for (j = 0; j < 5; j++)
       {
-        $(".board-row-" + i + "").append("<td class='cell-row-" + i + " cell-column-" + j + "'> 1 </td>");
+        $(".board-row-" + i + "").append("<td class='cell-row-" + i + " cell-column-" + j + "'>  </td>");
         boardMatrix[i][j] = [];
         boardMatrix[i][j] = false;
       }
@@ -34,10 +34,12 @@ $( document ).ready(function() {
     do {
       row = Math.floor(Math.random() * 7);
       col = Math.floor(Math.random() * 5);
-      if (boardMatrix[row][col] == true)
+      if (boardMatrix[row][col] === true)
         continue;
       boardMatrix[row][col] = true;
       $(".cell-row-"+row+".cell-column-"+col+"").addClass("bomb");
+  //    $(".bomb").css({"background-color": "yellow"});
+
 
 
       plantedMines++;
@@ -51,6 +53,76 @@ $( document ).ready(function() {
     var rowIndex = $(this).parent('tr').prevAll().length;
 });
 
+function calcAdjBombs ()  {
+  var numBombs;
+    for (i = 0; i < 7; i++) {
+      for (j = 0; j < 5; j++)
+      {
+        numBombs = 0;
+
+        if (i !== 0 && j !== 0)
+          if (boardMatrix[i-1][j-1] === true) {
+            numBombs++;
+          }
+
+        if (i !== 0)
+          if (boardMatrix[i-1][j] === true) {
+            numBombs++;
+         }
+        if (j !== 0)
+          if (boardMatrix[i][j-1] === true) {
+            numBombs++;
+}
+        if (j !== 0 && i !== 6)
+          if (boardMatrix[i+1][j-1] === true) {
+            numBombs++;
+          }
+
+        if (i !== 0 && j !== 4)
+          if (boardMatrix[i-1][j+1] === true) {
+            numBombs++;
+          }
+
+        if (i !== 6 && j !== 4)
+          if (boardMatrix[i+1][j+1] === true) {
+            numBombs++;
+}
+        if (i !== 6)
+          if (boardMatrix[i+1][j] === true) {
+            numBombs++;
+}
+        if (j !== 4)
+          if (boardMatrix[i][j+1] === true) {
+            numBombs++;
+}
+
+/////// Append to TD
+
+  if (boardMatrix[i][j] !== true) {
+  $(".cell-row-"+i+".cell-column-"+j+"").append("<p>"+numBombs+"</p>");
+  boardMatrix[i][j] = numBombs;
+}
+  $("p").hide();
+
+      }
+
+    }
+
+
+}
+
+function displayClicked (row,col) {
+
+
+}
+
+$("body").on("click", "td", function() {
+     var col = $(this).parent().children().index($(this));
+     var row = $(this).parent().parent().children().index($(this).parent());
+     displayClicked(row,col);
+   });
+
   drawBoard();
   plantMines();
+  calcAdjBombs();
 });
