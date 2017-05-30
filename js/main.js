f5fb169f346c3c3b853b52357a8a614fe0f35a78
Matthew-Wroblewski <wroblewski.m@zoho.com>
2017-05-30 03:10:08 -0400

    added mouse click reveal logic

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $( document ).ready(function() {
   numMines = 7;
   plantedMines = 0;
   var boardMatrix = [];
+  var revealedMatrix = [];
 
 
   function drawBoard () {
@@ -15,17 +16,19 @@ $( document ).ready(function() {
     {
       $("table").append("<tr class='board-row-" + i + "'></tr>");
       boardMatrix[i] = [];
+      revealedMatrix[i] = [];
 
       for (j = 0; j < 5; j++)
       {
         $(".board-row-" + i + "").append("<td class='cell-row-" + i + " cell-column-" + j + "'>  </td>");
         boardMatrix[i][j] = [];
+        revealedMatrix[i][j] = [];
         boardMatrix[i][j] = false;
+        revealedMatrix[i][j] = false;
       }
       $("tr").append("</tr>");
 
     }
-
 
   }
 
@@ -35,10 +38,10 @@ $( document ).ready(function() {
       row = Math.floor(Math.random() * 7);
       col = Math.floor(Math.random() * 5);
       if (boardMatrix[row][col] === true)
-        continue;
+      continue;
       boardMatrix[row][col] = true;
       $(".cell-row-"+row+".cell-column-"+col+"").addClass("bomb");
-  //    $(".bomb").css({"background-color": "yellow"});
+      //    $(".bomb").css({"background-color": "yellow"});
 
 
 
@@ -51,76 +54,169 @@ $( document ).ready(function() {
   $('td').click(function(){
     var colIndex = $(this).prevAll().length;
     var rowIndex = $(this).parent('tr').prevAll().length;
-});
+  });
 
-function calcAdjBombs ()  {
-  var numBombs;
+  function calcAdjBombs ()  {
+    var numBombs;
     for (i = 0; i < 7; i++) {
       for (j = 0; j < 5; j++)
       {
         numBombs = 0;
 
         if (i !== 0 && j !== 0)
-          if (boardMatrix[i-1][j-1] === true) {
-            numBombs++;
-          }
+        if (boardMatrix[i-1][j-1] === true) {
+          numBombs++;
+        }
 
         if (i !== 0)
-          if (boardMatrix[i-1][j] === true) {
-            numBombs++;
-         }
+        if (boardMatrix[i-1][j] === true) {
+          numBombs++;
+        }
         if (j !== 0)
-          if (boardMatrix[i][j-1] === true) {
-            numBombs++;
-}
+        if (boardMatrix[i][j-1] === true) {
+          numBombs++;
+        }
         if (j !== 0 && i !== 6)
-          if (boardMatrix[i+1][j-1] === true) {
-            numBombs++;
-          }
+        if (boardMatrix[i+1][j-1] === true) {
+          numBombs++;
+        }
 
         if (i !== 0 && j !== 4)
-          if (boardMatrix[i-1][j+1] === true) {
-            numBombs++;
-          }
+        if (boardMatrix[i-1][j+1] === true) {
+          numBombs++;
+        }
 
         if (i !== 6 && j !== 4)
-          if (boardMatrix[i+1][j+1] === true) {
-            numBombs++;
-}
+        if (boardMatrix[i+1][j+1] === true) {
+          numBombs++;
+        }
         if (i !== 6)
-          if (boardMatrix[i+1][j] === true) {
-            numBombs++;
-}
+        if (boardMatrix[i+1][j] === true) {
+          numBombs++;
+        }
         if (j !== 4)
-          if (boardMatrix[i][j+1] === true) {
-            numBombs++;
-}
+        if (boardMatrix[i][j+1] === true) {
+          numBombs++;
+        }
 
-/////// Append to TD
+        /////// Append to TD
 
-  if (boardMatrix[i][j] !== true) {
-  $(".cell-row-"+i+".cell-column-"+j+"").append("<p>"+numBombs+"</p>");
-  boardMatrix[i][j] = numBombs;
-}
-  $("p").hide();
+        if (boardMatrix[i][j] !== true) {
+          $(".cell-row-"+i+".cell-column-"+j+"").append("<p>"+numBombs+"</p>");
+          boardMatrix[i][j] = numBombs;
+        }
+        $("p").hide();
+
 
       }
 
     }
 
 
-}
+  }
 
-function displayClicked (row,col) {
+  function displayClicked (row,col) {
 
 
-}
+    if (boardMatrix[row][col] !== 0) {
+      console.log("hi");
+      revealedMatrix[row][col] = true;
+      $(".cell-row-"+row+".cell-column-"+col+" > p").show();
+    }
 
-$("body").on("click", "td", function() {
-     var col = $(this).parent().children().index($(this));
-     var row = $(this).parent().parent().children().index($(this).parent());
-     displayClicked(row,col);
-   });
+   else {
+    if (row !== 0 && col !== 0 && revealedMatrix[row-1][col-1] === false)
+    {
+      revealedMatrix[row-1][col-1] = true;
+      if (boardMatrix[row-1][col-1] === 0) {
+        $(".cell-row-"+(row-1)+".cell-column-"+(col-1)+"").addClass("noBomb");
+        displayClicked(row-1,col-1);
+      }
+      else  $(".cell-row-"+(row-1)+".cell-column-"+(col-1)+" > p").show();
+    }
+
+    if (row !== 0 && revealedMatrix[row-1][col] === false)
+    {
+      revealedMatrix[row-1][col] = true;
+
+      if (boardMatrix[row-1][col] === 0) {
+        $(".cell-row-"+(row-1)+".cell-column-"+col+"").addClass("noBomb");
+        displayClicked(row-1,col);
+      }
+      else  $(".cell-row-"+(row-1)+".cell-column-"+col+" > p").show();
+    }
+    if (col !== 0  && revealedMatrix[row][col-1] === false)
+    {
+      revealedMatrix[row][col-1] = true;
+
+      if (boardMatrix[row][col-1] === 0) {
+      $(".cell-row-"+row+".cell-column-"+(col-1)+"").addClass("noBomb");
+        displayClicked(row,col-1);
+      }
+      else  $(".cell-row-"+(row)+".cell-column-"+(col-1)+" > p").show();
+    }
+    if (row !== 6 && col !== 0  && revealedMatrix[row+1][col-1] === false )
+    {
+      revealedMatrix[row+1][col-1] = true;
+
+      if (boardMatrix[row+1][col-1] === 0) {
+        $(".cell-row-"+(row+1)+".cell-column-"+(col-1)+"").addClass("noBomb");
+        displayClicked(row+1,col-1);
+      }
+      else $(".cell-row-"+(row+1)+".cell-column-"+(col-1)+" > p").show();
+    }
+
+    if (row !== 0 && col !== 4 && revealedMatrix[row-1][col+1] === false)
+    {
+      revealedMatrix[row-1][col+1] = true;
+
+      if (boardMatrix[row-1][col+1] === 0) {
+        $(".cell-row-"+(row-1)+".cell-column-"+(col+1)+"").addClass("noBomb");
+        displayClicked(row-1,col+1);
+      }
+      else $(".cell-row-"+(row-1)+".cell-column-"+(col+1)+" > p").show();
+    }
+
+    if (row !== 6 && col !== 4 && revealedMatrix[row+1][col+1] === false)
+    {
+      revealedMatrix[row+1][col+1] = true;
+
+      if (boardMatrix[row+1][col+1] === 0) {
+        $(".cell-row-"+(row+1)+".cell-column-"+(col+1)+"").addClass("noBomb");
+        displayClicked(row+1,col+1);
+      }
+      else $(".cell-row-"+(row+1)+".cell-column-"+(col+1)+" > p").show();
+    }
+    if (row !== 6  && revealedMatrix[row+1][col] === false)
+    {
+      revealedMatrix[row+1][col] = true;
+
+      if (boardMatrix[row+1][col] === 0) {
+        $(".cell-row-"+(row+1)+".cell-column-"+col+"").addClass("noBomb");
+        displayClicked(row+1,col);
+      }
+      else $(".cell-row-"+(row+1)+".cell-column-"+(col)+" > p").show();
+    }
+    if (col !== 4 && revealedMatrix[row][col+1] === false)
+    {
+      revealedMatrix[row][col+1] = true;
+
+      if (boardMatrix[row][col+1] === 0) {
+        $(".cell-row-"+row+".cell-column-"+(col+1)+"").addClass("noBomb");
+        displayClicked(row,col+1);
+    }
+      else  $(".cell-row-"+(row)+".cell-column-"+(col+1)+" > p").show();
+    }
+  }
+
+
+  }
+
+  $("body").on("click", "td", function() {
+    var col = $(this).parent().children().index($(this));
+    var row = $(this).parent().parent().children().index($(this).parent());
+    displayClicked(row,col);
+  });
 
   drawBoard();
   plantMines();
